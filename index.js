85b0dec2904d2006e627626ef5970468b05d5301
Rafi <imranhossainemon1717@gmail.com>
2023-04-30 23:12:34 +0600

    database connect

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-const express = require('express');
-require('dotenv').config();
+const express = require("express");
+const connect = require("./config/db");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get('/',(req,res)=>{
-    res.json({msg:'wellcome to bongobazaar!!.'});
+// Databse Connection
+connect();
+
+app.get("/", (req, res) => {
+  res.json({ msg: "wellcome to bongobazaar!!." });
 });
 
-app.listen(port,()=> {
-    console.log(`BongoBazaar Server running on ${port}`)
+app.listen(port, () => {
+  console.log(`BongoBazaar Server running on ${port}`);
 });
