f46da8450dee2d846c6f53810d38efac50c066c5
Rafi <imranhossainemon1717@gmail.com>
2023-05-04 03:06:44 +0600

    create user and jwt token create

---
+++ Auth/auth.js
@@ -0,0 +1,12 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+module.exports.hashedPassword = async (password) => {
+  const salt = await bcrypt.genSalt(10);
+  const hashed = await bcrypt.hash(password, salt);
+  return hashed;
+};
+
+module.exports.createToken = (user) => {
+  return jwt.sign(user, process.env.JWT_SECRET, { expiresIn: "7d" });
+};
--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -1,9 +1,35 @@
 const { validationResult } = require("express-validator");
+const UserModel = require("../models/User");
+const { hashedPassword, createToken } = require("../Auth/auth");
 
-module.exports.register = (req, res) => {
+module.exports.register = async (req, res) => {
   const errors = validationResult(req);
 
-  if (!errors.isEmpty()) {
-    console.log(errors.array());
+  if (errors.isEmpty()) {
+    const { name, email, password } = req.body;
+    try {
+      const emailExist = await UserModel.findOne({ email });
+      if (!emailExist) {
+        const hashed = await hashedPassword(password);
+        const user = await UserModel.create({
+          name,
+          email,
+          password: hashed,
+        });
+        const token = createToken({ id: user._id, name: user.name });
+        return res
+          .status(201)
+          .json({ msg: "User Created Successfully!", token });
+      } else {
+        return res
+          .status(401)
+          .json({ errors: [{ msg: `${email} has been already taken.` }] });
+      }
+    } catch (error) {
+      console.log(error.message);
+      return res.status(500).json("Server Internal Error");
+    }
+  } else {
+    return res.status(400).json(errors.array());
   }
 };
