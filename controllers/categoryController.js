41e77a606e56c4963bca483fd5f0b87e4e22a760
Rafi <imranhossainemon1717@gmail.com>
2023-05-13 22:23:30 +0600

    create single category

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -43,3 +43,15 @@ module.exports.categories = async (req, res) => {
     return res.status(500).json("Internal Server Error.");
   }
 };
+
+// fetch single category
+
+module.exports.fetchCategory = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const response = await CategoryModel.findOne({ _id: id });
+    return res.status(200).json({ category: response });
+  } catch (error) {
+    return res.status(500).json("server Internal Error");
+  }
+};
--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const { categoryValidation } = require("../validations/categoryValidation");
-const { createCategory, categories } = require("../controllers/categoryController");
+const {
+  createCategory,
+  categories,
+  fetchCategory,
+} = require("../controllers/categoryController");
 const { authorized } = require("../Auth/Authorization");
 const router = express.Router();
 
-router.post("/create-category", [categoryValidation,authorized], createCategory);
-router.get("/categories/:page",categories);
+router.post(
+  "/create-category",
+  [categoryValidation, authorized],
+  createCategory
+);
+router.get("/categories/:page", authorized, categories);
+router.get("/fetch-category/:id", authorized, fetchCategory);
 
 module.exports = router;
